7d29dcaf774aff0bf643423d3a30d3882a2eb452
uni897 <akhasmeer7@gmail.com>
2025-10-31 14:01:02 -0400

    homiyak

--- vite-project/src/main.js
+++ vite-project/src/main.js
@@ -1,1 +1,14 @@
 import './style.css'
+
+
+//window.location.href = '//file.html'
+/*
+const button = document.getElementById('BUTTON')
+button.addEventListener('click', function() {
+    window.location.href = '\\file.html'
+});
+*/
+const button = document.getElementById(".waol")
+button.addEventListener("click", function(){
+    document.getElementById("h1").style.color=""
+})
