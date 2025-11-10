2822af1e2c176453620f71f864e7306b00553ef3
uni897 <akhasmeer7@gmail.com>
2025-11-10 14:00:42 -0500

    ccxcvzxcv

--- vite-project/src/main.js
+++ vite-project/src/main.js
@@ -8,7 +8,12 @@ button.addEventListener('click', function() {
     window.location.href = '\\file.html'
 });
 */
-const button = document.getElementById(".waol")
-button.addEventListener("click", function(){
-    document.getElementById("h1").style.color=""
-})
+document.querySelector(".waol").addEventListener("click", function () {
+  if (document.body.classList.contains("cool")) {
+    document.body.classList.add("warm");
+    document.body.classList.remove("cool");
+  } else {
+    document.body.classList.add("cool");
+    document.body.classList.remove("warm");
+  }
+});
