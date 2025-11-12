e31a11d6abc5f97d2c5ed861b27dc82fd5ffea6d
uni897 <akhasmeer7@gmail.com>
2025-11-12 14:02:10 -0500

    ngfdgfdgdsagds

--- vite-project/src/main.js
+++ vite-project/src/main.js
@@ -3,19 +3,20 @@ import './style.css'
 
 //window.location.href = '//file.html'
 
-const button = document.querySelector('.waol')
+//beginning page js
+const button = document.querySelector('.change')
 button.addEventListener('click', () => {
     window.location.href = '/trivia.html'
 });
 
-/*
 document.querySelector(".waol").addEventListener("click", function () {
-  if (document.body.classList.contains("cool")) {
-    document.body.classList.add("warm");
-    document.body.classList.remove("cool");
+  if (document.body.classList.contains("green")) {
+    document.body.classList.add("red");
+    document.body.classList.remove("green");
   } else {
-    document.body.classList.add("cool");
-    document.body.classList.remove("warm");
+    document.body.classList.add("green");
+    document.body.classList.remove("red");
   }
 });
-*/
+
+//trivia page js
